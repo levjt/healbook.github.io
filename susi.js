ea795ef6651643692b699a60e2de563927543317
levjt <akdie2011@gmail.com>
2021-07-26 17:59:59 +0700

    upload

---
+++ susi.js
@@ -0,0 +1,10 @@
+
+
+function toggle() {
+  document.getElementById("container").classList.toggle("signin");
+  document.getElementById("container").classList.toggle("signup");
+}
+function addclass() {
+    container.classList.add("signin");
+}
+setTimeout(addclass(),200)
